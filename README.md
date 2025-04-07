615345ef4996337d1332c379ae896289b44518e3
wynnhowe.lv <wynnhome.lv@gmail.com>
2025-04-08 01:03:14 +0800

    修改MD5使用方式为: md5 [file|strings] ...

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # md5
 C语言实现的MD5算法源码
 ```
-usage:
-    ./md5 -f file ...
-    ./md5 string ...
-```
+Usage:
+    ./md5  [file|string] ...
+ ```
--- main.c
+++ main.c
@@ -3,64 +3,71 @@
 #include <string.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <sys/stat.h>
 #include "md5.h"
+
+bool IsFile(const char *fileOrStr)
+{
+  struct stat fileStat;
+  int ret = stat(fileOrStr, &fileStat);
+  if (ret) {
+    return false;
+  }
+  
+  return S_ISREG(fileStat.st_mode);
+}
+
 
 int main(int argc, char *argv[]) {
 	int i, n;
 	bool isfile = false;
 	unsigned char decrypt[16];
 	MD5_CTX md5;
-	
-	if(argc > 1 && strlen(argv[1]) == 2 && !memcmp(argv[1], "-f", 2)) {
-		isfile = true;
-	}
-	
-	if(argc == 1 || (argc == 2 && isfile)) {
-		fprintf(stderr, "usage:\n    %s -f file ...\n    %s string ...\n", argv[0], argv[0]);
-		return 1;
-	}
-	
-	if(isfile) {
+
+    if (argc == 1) {
+      fprintf(stderr, "Usage:\n    %s [file|string] ...\n", argv[0]);
+      exit(1);
+    }
+
+    for (int i = 1; i < argc; i++) {
+      if(IsFile(argv[i])) {
 		FILE *fp;
-		for(n=2; n<argc; n++) {
-			fp = fopen(argv[n], "rb");
-			if(fp == NULL) {
-				fprintf(stderr, "File %s not exists, errno = %d, error = %s\n", argv[n], errno, strerror(errno));
-				continue;
-			}
+        fp = fopen(argv[i], "rb");
+        if(fp == NULL) {
+          fprintf(stderr, "File %s not exists, errno = %d, error = %s\n", argv[i], errno, strerror(errno));
+          continue;
+        }
 			
-			MD5Init(&md5);
+        MD5Init(&md5);
+        do {
+          unsigned char encrypt[1024];
+          while(!feof(fp)) {
+            MD5Update(&md5, encrypt, fread(encrypt, 1, sizeof(encrypt), fp));
+          }
+          fclose(fp);
+        } while(0);
 			
-			do {
-				unsigned char encrypt[1024];
-				while(!feof(fp)) {
-					MD5Update(&md5, encrypt, fread(encrypt, 1, sizeof(encrypt), fp));
-				}
-				fclose(fp);
-			} while(0);
-			
-			MD5Final(&md5, decrypt);
+        MD5Final(&md5, decrypt);
 
-			for(i=0; i<16; i++) {
-				printf("%02x", decrypt[i]);  //02x前需要加上 %
-			}
+        for(int j = 0; j < 16; j++) {
+          printf("%02x", decrypt[j]);
+        }
 			
-			printf("  %s\n", argv[n]);
-		}
-	} else {
-		for(n=1; n<argc; n++) {
-			MD5Init(&md5);
-			MD5Update(&md5, (unsigned char *)argv[n], strlen(argv[n]));
-			MD5Final(&md5, decrypt);
+        printf("  %s\n", argv[i]);
+      } else {
+        MD5Init(&md5);
+        MD5Update(&md5, (unsigned char *)argv[i], strlen(argv[i]));
+        MD5Final(&md5, decrypt);
 
-			for(i=0; i<16; i++) {
-				printf("%02x", decrypt[i]);  //02x前需要加上 %
-			}
+        for(int j=0; j < 16; j++) {
+          printf("%02x", decrypt[j]);
+        }
 			
-			printf("  %s\n", argv[n]);
-		}
-	}
+        printf("  %s\n", argv[i]);
 
+      }
+
+    }
 	return 0;
 }  
 
